b4af1dd0e12ad2ab47ccc54533c071605617eed5
krypticwolf3 <krypticwolf3@users.noreply.github.com>
2016-03-03 14:52:33 -0500

    Reverting the sprite changes to fix game not showing.

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ window.onload = function() {
     function preload () {
 
         game.load.atlas('tank', 'assets/tanks/tanks.png', 'assets/tanks/tanks.json');
-        game.load.atlas('enemy', 'assets/tanks/tanks.png', 'assets/tanks/tanks.json');
+        game.load.atlas('enemy', 'assets/tanks/enemy-tanks.png', 'assets/tanks/tanks.json');
         game.load.image('elements', 'assets/tanks/elem_earth.png');
         game.load.image('logo', 'assets/tanks/logo.png');
         game.load.image('bullet', 'assets/tanks/bullet.png');
