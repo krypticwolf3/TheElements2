5ed4492fae79661bc97e108806dd5ecb2bfe5eac
krypticwolf3 <krypticwolf3@users.noreply.github.com>
2016-03-04 12:15:54 -0500

    Trying to get the original example to work again

--- js/main.js
+++ js/main.js
@@ -94,12 +94,12 @@ window.onload = function() {
 
   function preload () {
 
-    game.load.atlas('tank', 'assets/games/tanks/tanks.png', 'assets/games/tanks/tanks.json');
-    game.load.atlas('enemy', 'assets/games/tanks/enemy-tanks.png', 'assets/games/tanks/tanks.json');
-    game.load.image('logo', 'assets/games/tanks/logo.png');
-    game.load.image('bullet', 'assets/games/tanks/bullet.png');
-    game.load.image('earth', 'assets/games/tanks/scorched_earth.png');
-    game.load.spritesheet('kaboom', 'assets/games/tanks/explosion.png', 64, 64, 23);
+    game.load.atlas('tank', 'assets/tanks/tanks.png', 'assets/games/tanks/tanks.json');
+    game.load.atlas('enemy', 'assets/tanks/enemy-tanks.png', 'assets/games/tanks/tanks.json');
+    game.load.image('logo', 'assets/tanks/logo.png');
+    game.load.image('bullet', 'assets/tanks/bullet.png');
+    game.load.image('earth', 'assets/tanks/scorched_earth.png');
+    game.load.spritesheet('kaboom', 'assets/tanks/explosion.png', 64, 64, 23);
     
   }
 
